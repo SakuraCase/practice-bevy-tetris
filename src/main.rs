d6c9516a57246d7136b0ed9b29addaa4ef9d23ff
SakuraCase <sakurada6617@gmail.com>
2023-01-29 16:56:49 +0900

    step4

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ const Y_LENGTH: u32 = 18;
 const SCREEN_WIDTH: u32 = UNIT_WIDTH * X_LENGTH;
 const SCREEN_HEIGHT: u32 = UNIT_HEIGHT * Y_LENGTH;
 
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -19,17 +25,34 @@ fn main() {
             ..default()
         }))
         .add_startup_system(setup)
+        .add_system(position_transform)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(20.0, 20.0)),
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite { ..default() },
             ..default()
         },
-        ..default()
-    });
+        Position { x: 1, y: 5 },
+    ));
+}
+
+fn position_transform(mut position_query: Query<(&Position, &mut Transform, &mut Sprite)>) {
+    let origin_x = UNIT_WIDTH as i32 / 2 - SCREEN_WIDTH as i32 / 2;
+    let origin_y = UNIT_HEIGHT as i32 / 2 - SCREEN_HEIGHT as i32 / 2;
+
+    position_query
+        .iter_mut()
+        .for_each(|(pos, mut transform, mut sprite)| {
+            transform.translation = Vec3::new(
+                (origin_x + pos.x * UNIT_WIDTH as i32) as f32,
+                (origin_y + pos.y * UNIT_HEIGHT as i32) as f32,
+                0.0,
+            );
+            sprite.custom_size = Some(Vec2::new(UNIT_WIDTH as f32, UNIT_HEIGHT as f32))
+        });
 }
